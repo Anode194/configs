0f131f51f612bbc5d5435a30dd01161fea680fa7
Jo Phillips <joxphillips@gmail.com>
2020-12-29 10:38:41 -0700

    add templates & update goals for 2021.

---
+++ nvim/nvim/.skeleton.java
@@ -0,0 +1,14 @@
+/*****************************************************
+ * created by: Jo Phillips
+ * email: joxphillips@gmail.com
+ *  
+ *
+ ****************************************************/
+
+public class hello //rename!
+{
+    public static void main(String[] args)
+    {
+        system.out.println("Hello, World");
+    }
+}
